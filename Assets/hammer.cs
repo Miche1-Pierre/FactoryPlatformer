2c03613c93b1d47c76f2dc33fe73a617bf24aea8
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-01-30 11:44:14 +0100

    Ajout des collision des hamers

--- Assets/hammer.cs
+++ Assets/hammer.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
-
-public class hammer : MonoBehaviour
+public class HammerColliderControl : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private BoxCollider2D boxCollider;
+    public float speed = 1.54f;
+    public float maxHeight = 1f;
+
+    private float currentHeight = 0f;
+    private bool movingDown = true;
+
     void Start()
     {
-        
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (movingDown)
+        {
+            currentHeight -= speed * Time.deltaTime;
+            if (currentHeight <= -maxHeight)
+            {
+                movingDown = false;
+            }
+        }
+        else
+        {
+            currentHeight += speed * Time.deltaTime;
+            if (currentHeight >= 0)
+            {
+                movingDown = true;
+            }
+        }
+
+        boxCollider.size = new Vector2(boxCollider.size.x, Mathf.Abs(currentHeight));
+        boxCollider.offset = new Vector2(boxCollider.offset.x, currentHeight / 2f);
     }
 }
